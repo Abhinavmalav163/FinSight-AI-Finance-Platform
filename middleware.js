adb8c1349fa1bbf3071983e7cd404fb90fd90e4d
Abhinav Malav <abhinavmalav1468@gmail.com>
2025-11-13 21:04:34 +0530

    preparing for deployment

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
 
 const isProtectedRoute = createRouteMatcher([
   "/main/dashboard(.*)",
@@ -9,24 +9,7 @@ const isProtectedRoute = createRouteMatcher([
   "/api/webhooks/clerk",
 ]);
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    shield({
-      mode: 'LIVE'
-    }),
-    detectBot({
-      mode: "LIVE",
-      allow:[
-        "CATEGORY:SEARCH_ENGINE", "GO_HTTP_CLIENT"],
-      
-    })
-  ]
-})
-
-// Create base Clerk middleware
-const clerkMiddlewareHandler = clerkMiddleware(async (auth, req) => {
-  // Add this condition to skip Inngest routes from all middleware
+const clerkHandler = clerkMiddleware(async (auth, req) => {
   if (req.nextUrl.pathname.startsWith("/api/inngest")) {
     return NextResponse.next();
   }
@@ -41,14 +24,11 @@ const clerkMiddlewareHandler = clerkMiddleware(async (auth, req) => {
   return NextResponse.next();
 });
 
-export default createMiddleware(aj, clerkMiddlewareHandler);
+export default clerkHandler;
 
-// The matcher is simplified as the Inngest route is now handled inside the middleware.
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
